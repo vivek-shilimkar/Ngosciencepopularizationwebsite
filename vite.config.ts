ccdddb4738ddab71a1e7e492cbaec692c56b95b6
Vivek Shilimkar <vivek-shilimkar@users.noreply.github.com>
2026-01-16 10:37:36 +0530

    git add all changed files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/Ngosciencepopularizationwebsite/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
